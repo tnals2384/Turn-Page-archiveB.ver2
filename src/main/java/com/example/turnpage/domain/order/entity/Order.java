d6d8b8636581cfe70b9594f1979283e08bd45fb0
bflykky <kkyxw@naver.com>
2024-02-25 22:56:38 +0900

    fix: Order 엔티티 클래스에 대응하는 테이블명 orders로 변경

--- src/main/java/com/example/turnpage/domain/order/entity/Order.java
+++ src/main/java/com/example/turnpage/domain/order/entity/Order.java
@@ -12,6 +12,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -21,6 +22,7 @@ import org.hibernate.annotations.SQLRestriction;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
+@Table(name = "orders")
 @SQLDelete(sql = "UPDATE order SET deleted_at = CURRENT_TIMESTAMP WHERE order_id = ?")
 @SQLRestriction("deleted_at is NULL")
 public class Order extends BaseTimeEntity {
